cffbaf548ebdd6efaddd4e309b4e85e318e0343d
Motorina Arina <arinamotorinaaaaa@gmail.com>
2021-10-13 22:20:57 +0300

    done

--- Cars/main.cpp
+++ Cars/main.cpp
@@ -17,10 +17,10 @@ class Car {
 public:
     virtual ~Car(){};
     virtual string UsefulCar() const = 0;
-    virtual int colorOfCar() const = 0;
-    virtual int speedOfCar()  = 0;
-    virtual int priceOfCar() = 0;
-    virtual bool whatIsSalon() = 0;
+    virtual string colorOfCar() const = 0;
+    virtual int speedOfCar() const = 0;
+    virtual int priceOfCar() const = 0;
+    virtual bool whatIsSalon() const = 0;
 };
 
 /**
@@ -33,17 +33,17 @@ public:
     string UsefulCar() const override {
       return "The Audi Car has created";
     }
-    int colorOfCar() const override {
-      return WHITE;
+    string colorOfCar() const override {
+      return "WHITE";
     }
-    int speedOfCar() override {
-      return 150;
+    int speedOfCar() const override {
+      return 100;
     }
-    int priceOfCar() override {
-      return 3000000;
+    int priceOfCar() const override {
+      return 1000000;
     }
-    bool whatIsSalon() override {
-      return EXPENSIVE;
+    bool whatIsSalon() const override {
+      return POOR;
     }
     
 };
@@ -52,18 +52,37 @@ public:
     string UsefulCar() const override {
       return "The Audi Car type 1 has created";
     }
-//    int colorOfCar() override {
-//      return WHITE;
-//    }
-//    int speedOfCar() override {
-//      return 150;
-//    }
-//    int priceOfCar() override {
-//      return 3000000;
-//    }
-//    bool whatIsSalon() override {
-//      return EXPENSIVE;
-//    }
+    string colorOfCar() const override {
+      return "WHITE";
+    }
+    int speedOfCar() const override {
+      return 150;
+    }
+    int priceOfCar() const override {
+      return 3000000;
+    }
+    bool whatIsSalon() const override {
+      return EXPENSIVE;
+    }
+    
+};
+class AudiCar2 : public AudiCar {
+public:
+    string UsefulCar() const override {
+      return "The Audi Car type 2 has created";
+    }
+    string colorOfCar() const override {
+      return "RED";
+    }
+    int speedOfCar() const override {
+      return 160;
+    }
+    int priceOfCar() const override {
+      return 3100005;
+    }
+    bool whatIsSalon() const override {
+      return POOR;
+    }
     
 };
 
@@ -71,34 +90,51 @@ class BMWCar : public Car {
     string UsefulCar() const override {
       return "The BMW Car has created";
     }
-//    int colorOfCar() override {
-//      return WHITE;
-//    }
-//    int speedOfCar() override {
-//      return 150;
-//    }
-//    int priceOfCar() override {
-//      return 3000000;
-//    }
-//    bool whatIsSalon() override {
-//      return EXPENSIVE;
-//    }
+    string colorOfCar() const override {
+      return "WHITE";
+    }
+    int speedOfCar() const override {
+      return 100;
+    }
+    int priceOfCar() const override {
+      return 1000000;
+    }
+    bool whatIsSalon() const override {
+      return POOR;
+    }
 };
 class BMWCar1 : public BMWCar {
     string UsefulCar() const override {
       return "The BMW Car type 1 has created";
     }
-    int colorOfCar() const override {
-      return BLACK;
+    string colorOfCar() const override {
+      return "BLACK";
     }
-    int speedOfCar() override {
+    int speedOfCar() const override {
       return 200;
     }
-    int priceOfCar() override {
+    int priceOfCar() const override {
       return 5500000;
     }
-    bool whatIsSalon() override {
-      return POOR;
+    bool whatIsSalon() const override {
+      return EXPENSIVE;
+    }
+};
+class BMWCar2 : public BMWCar {
+    string UsefulCar() const override {
+      return "The BMW Car type 2 has created";
+    }
+    string colorOfCar() const override {
+      return WHITE;
+    }
+    int speedOfCar() const override {
+      return 190;
+    }
+    int priceOfCar() const override {
+      return 5300000;
+    }
+    bool whatIsSalon() const override {
+      return EXPENSIVE;
     }
 };
 
@@ -107,54 +143,59 @@ class BMWCar1 : public BMWCar {
  * с другом, но правильное взаимодействие возможно только между продуктами одной
  * и той же конкретной вариации.
  */
-//class Autobus {
-//  /**
-//   * Продукт B способен работать самостоятельно...
-//   */
-// public:
-//  virtual ~Autobus(){};
-//  virtual string UsefulBus() const = 0;
-//  /**
-//   * ...а также взаимодействовать с Продуктами A той же вариации.
-//   *
-//   * Абстрактная Фабрика гарантирует, что все продукты, которые она создает,
-//   * имеют одинаковую вариацию и, следовательно, совместимы.
-//   */
-//  virtual string AnotherUsefulBus(const Car &collaborator) const = 0;
-//};
+class Autobus {
+public:
+    virtual ~Autobus(){};
+    virtual string UsefulBus() const = 0;
+    //virtual string colorOfBus() const = 0;
+    virtual int countOfSeats() const = 0;
+    virtual string AnotherUsefulBus(const Car &collaborator) const = 0;
+};
 
 /**
  * Конкретные Продукты создаются соответствующими Конкретными Фабриками.
  */
-//class AudiBus : public Autobus {
-// public:
-//    string UsefulBus() const override {
-//    return "The Audi Bus has created";
-//  }
-//  /**
-//   * Продукт B1 может корректно работать только с Продуктом A1. Тем не менее, он
-//   * принимает любой экземпляр Абстрактного Продукта А в качестве аргумента.
-//   */
-//  std::string AnotherUsefulBus(const Car &collaborator) const override {
-//    const string result = collaborator.UsefulCar();
-//    return "The result of the B1 collaborating with ( " + result + " )";
-//  }
-//};
+class AudiBus : public Autobus {
+public:
+    string UsefulBus() const override {
+        return "The Audi Bus has created";
+    }
+    int countOfSeats() const override {
+        return 28;
+    }
+  /**
+   * Продукт B1 может корректно работать только с Продуктом A1. Тем не менее, он
+   * принимает любой экземпляр Абстрактного Продукта А в качестве аргумента.
+   */
+  string AnotherUsefulBus(const Car &collaborator) const override {
+    const string result = collaborator.colorOfCar();
+      return "Audi bus was created with ( " + result + " )";
+  }
+};
 //
-//class BMWBus : public Autobus {
-// public:
-//    string UsefulBus() const override {
-//    return "The BMW Bus has created";
-//  }
-//  /**
-//   * Продукт B2 может корректно работать только с Продуктом A2. Тем не менее, он
-//   * принимает любой экземпляр Абстрактного Продукта А в качестве аргумента.
-//   */
-//  std::string AnotherUsefulBus(const Car &collaborator) const override {
-//    const std::string result = collaborator.UsefulCar();
-//    return "The result of the B2 collaborating with ( " + result + " )";
-//  }
-//};
+class BMWBus : public Autobus {
+public:
+    string UsefulBus() const override {
+        return "The BMW Bus has created";
+    }
+    int countOfSeats() const override {
+        return 34;
+    }
+  /**
+   * Продукт B1 может корректно работать только с Продуктом A1. Тем не менее, он
+   * принимает любой экземпляр Абстрактного Продукта А в качестве аргумента.
+   */
+  string AnotherUsefulBus(const Car &collaborator) const override {
+    const string result = collaborator.colorOfCar();
+      return "BMW bus was created with ( " + result + " )";
+  }
+  /**
+   * Продукт B2 может корректно работать только с Продуктом A2. Тем не менее, он
+   * принимает любой экземпляр Абстрактного Продукта А в качестве аргумента.
+   */
+
+  
+};
 
 /**
  * Интерфейс Абстрактной Фабрики объявляет набор методов, которые возвращают
@@ -165,9 +206,14 @@ class BMWCar1 : public BMWCar {
  */
 class AbstractFactory {
  public:
-    virtual Car *CreateTypeCar() const = 0;
-    virtual Car *CreateCar() const = 0;
-  //  virtual Autobus *CreateBus() const = 0;
+    virtual Car *CreateCarA() const = 0;
+    virtual Car *CreateCarB() const = 0;
+    virtual Car *CreateCarA1() const = 0;
+    virtual Car *CreateCarA2() const = 0;
+    virtual Car *CreateCarB1() const = 0;
+    virtual Car *CreateCarB2() const = 0;
+    virtual Autobus *CreateBusB() const = 0;
+    virtual Autobus *CreateBusA() const = 0;
 };
 
 /**
@@ -178,40 +224,47 @@ class AbstractFactory {
  */
 class ConcreteFactory1 : public AbstractFactory {
 public:
-    Car *CreateTypeCar() const override {
-      return new AudiCar1();
+    Car *CreateCarA() const override {
+      return new AudiCar();
+    }
+    Car *CreateCarB() const override {
+      return new BMWCar();
     }
     
-    Car *CreateCar() const override {
-        return new AudiCar();
+    Car *CreateCarA1() const override {
+        return new AudiCar1();
     }
-//    Autobus *CreateBus() const override {
-//        return new AudiBus();
-//    }
+    Car *CreateCarA2() const override {
+        return new AudiCar2();
+    }
+    Car *CreateCarB1() const override {
+        return new BMWCar1();
+    }
+    Car *CreateCarB2() const override {
+        return new BMWCar2();
+    }
+    Autobus *CreateBusA() const override {
+        return new AudiBus();
+    }
+    Autobus *CreateBusB() const override {
+        return new BMWBus();
+    }
+
 };
-//
-//class ConcreteFactoryType1 : public ConcreteFactory1 {
-// public:
-//  Car *CreateTypeCar() const override {
-//    return new AudiCar1();
-//  }
-//  Autobus *CreateTypeBus() const override {
-//    return new AudiBus();
-//  }
-//};
+
 
 /**
  * Каждая Конкретная Фабрика имеет соответствующую вариацию продукта.
  */
-//class ConcreteFactory2 : public AbstractFactory {
-// public:
-//  Car *CreateCar() const override {
-//   // return new BMWCar();
-//  }
-//  Autobus *CreateBus() const override {
-//    return new BMWBus();
-//  }
-//};
+class ConcreteFactory2 : public AbstractFactory {
+public:
+    Autobus *CreateBusA() const override {
+        return new AudiBus();
+    }
+    Autobus *CreateBusB() const override {
+        return new BMWBus();
+    }
+};
 
 /**
  * Клиентский код работает с фабриками и продуктами только через абстрактные
@@ -220,29 +273,96 @@ public:
  */
 
 void ClientCode(const AbstractFactory &factory) {
-    const Car *car = factory.CreateCar();
-    const Car *car1 = factory.CreateTypeCar();
- // const Autobus *bus = factory.CreateBus();
-    cout << car->UsefulCar() << "\n";
-    cout << car1->UsefulCar() <<" "<< car1->colorOfCar() << "\n";
-//  std::cout << bus->AnotherUsefulBus(*car) << "\n";
-    delete car;
-    delete car1;
-  //delete bus;
+    int anser = 0;
+    cout << "Would you like to buy a Car(1) or Bus(2)?" << endl;
+    cin >> anser;
+    switch(anser) {
+        case 1: {
+            int anser1 = 0;
+            cout << "Would you like to buy a Audi(1) or BMW(2)?" << endl;
+            cin >> anser1;
+            switch(anser1) {
+                case 1: {
+                    int anser11 = 0;
+//                    const Car *car = factory.CreateCarA();
+//                    cout << car->UsefulCar() << "\n";
+                    cout << "Would you like to buy a first complectation(1) or second complectation(2)?" << endl;
+                    cin >> anser11;
+                    switch(anser11) {
+                        case 1: {
+                            const Car *car1 = factory.CreateCarA1();
+                            cout << car1->UsefulCar() <<" with: \n"<< car1->colorOfCar() << "\n" << car1->colorOfCar() << "\n" << car1->speedOfCar() << "\n" << car1->priceOfCar() << "\n" << car1->whatIsSalon() << endl;
+                           // delete car;
+                            delete car1;break;
+                        }
+                        case 2: {
+                            const Car *car1 = factory.CreateCarA2();
+                            cout << car1->UsefulCar() <<" with: \n"<< car1->colorOfCar() << "\n" << car1->colorOfCar() << "\n" << car1->speedOfCar() << "\n" << car1->priceOfCar() << "\n" << car1->whatIsSalon() << endl;
+                           // delete car;
+                            delete car1;break;
+                        }
+                    }
+                  //  delete car;
+                    break;
+                }
+                case 2: {
+                    int anser12 = 0;
+
+                    cout << "Would you like to buy a first complectation(1) or second complectation(2)?" << endl;
+                    cin >> anser12;
+                    switch(anser12) {
+                        case 1: {
+                            const Car *car1 = factory.CreateCarB1();
+                            cout << car1->UsefulCar() <<" with: \n"<< car1->colorOfCar() << "\n" << car1->colorOfCar() << "\n" << car1->speedOfCar() << "\n" << car1->priceOfCar() << "\n" << car1->whatIsSalon() << endl;
+                           
+                            delete car1; break;
+                        }
+                        case 2: {
+                            const Car *car1 = factory.CreateCarB2();
+                            cout << car1->UsefulCar() <<" with: \n"<< car1->colorOfCar() << "\n" << car1->colorOfCar() << "\n" << car1->speedOfCar() << "\n" << car1->priceOfCar() << "\n" << car1->whatIsSalon() << endl;
+                            
+                            delete car1;break;
+                        }
+                    }
+                  //  delete car;
+                    break;
+                }
+            }
+            
+        }
+        case 2: {
+            cout << "Would you like to buy a Audi(1) or BMW(2)?" << endl;
+            int anserB = 0;
+            cin >> anserB;
+            switch(anserB) {
+                case 1: {
+                    const Autobus *bus1 = factory.CreateBusA();
+                    const Car *car = factory.CreateCarA();
+                    cout << bus1->UsefulBus() <<" with: \n" << bus1->countOfSeats() << " seats\n" << bus1->AnotherUsefulBus(*car) << endl;
+                    delete bus1;
+                    delete car;
+                    break;
+                }
+                case 2: {
+                    const Autobus *bus1 = factory.CreateBusB();
+                    const Car *car = factory.CreateCarB();
+                    cout << bus1->UsefulBus() <<" with: \n" << bus1->countOfSeats() << " seats\n" << bus1->AnotherUsefulBus(*car) << endl;
+                    delete bus1;
+                    delete car;
+                    break;
+                }
+            }
+            break;
+        }
+            
+    }
 }
 
 int main() {
-    std::cout << "Client: Testing client code with the first factory type:\n";
+    cout << "The dialog with customer: \n";
     ConcreteFactory1 *f1 = new ConcreteFactory1();
-  //  ConcreteFactoryType1 *f11 = new ConcreteFactoryType1();
     ClientCode(*f1);
-   // ClientCode(*f11);
     delete f1;
-//    delete f11;
-  std::cout << std::endl;
-//  std::cout << "Client: Testing the same client code with the second factory type:\n";
-//  ConcreteFactory2 *f2 = new ConcreteFactory2();
-//  ClientCode(*f2);
-//  delete f2;
   return 0;
 }
+
